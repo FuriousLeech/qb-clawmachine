285096028c9fff0340448aebbf963bf75421c585
FuriousLeech <46639826+FuriousLeech@users.noreply.github.com>
2022-02-28 20:34:02 -0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -16,7 +16,26 @@ Config.Prizes = { -- Add whatever items here you want as prizes
     'funkopop_moaningmertle',
     'funkopop_draco',
     'funkopop_voldemort',
-    'funkopop_snape'
+    'funkopop_snape',
+    'funkopop_icecube',
+    'funkopop_ironman',
+    'funkopop_jimhalpert',
+    'funkopop_myers',
+    'funkopop_ninja',
+    'funkopop_rick',
+    'funkopop_ronweasley',
+    'funkopop_sonic',
+    'funkopop_stanlee',
+    'funkopop_thanos',
+    'funkopop_thor',
+    'funkopop_wwoman',
+    'funkopop_woody',
+    'funkopop_venom',
+    'funkopop_willywonka',
+    'funkopop_groot',
+    'funkopop_hulk',
+    'funkopop_johnwick',
+    'funkopop_itachi'
 }
 
 Config.Text = {
